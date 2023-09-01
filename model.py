52d40225dad3e668b0d4d3bda401c304a80d590a
adelacvg <adelacvgaoiro@163.com>
2023-09-01 22:04:46 +0800

    fix model load

--- model.py
+++ model.py
@@ -874,16 +874,18 @@ class Trainer(object):
 
         data = torch.load(model_path, map_location=device)
 
-        model = self.accelerator.unwrap_model(self.model)
-        model.load_state_dict(data['model'])
-
         self.step = data['step']
-        # self.opt.load_state_dict(data['opt'])
-        # if self.accelerator.is_main_process:
-        #     self.ema.load_state_dict(data["ema"])
 
-        # if exists(self.accelerator.scaler) and exists(data['scaler']):
-        #     self.accelerator.scaler.load_state_dict(data['scaler'])
+        saved_state_dict = data['model']
+        model = self.accelerator.unwrap_model(self.model)
+        new_state_dict= {}
+        for k,v in saved_state_dict.items():
+            name=k[7:]
+            new_state_dict[name] = v
+        if hasattr(model, 'module'):
+            model.module.load_state_dict(new_state_dict)
+        else:
+            model.load_state_dict(new_state_dict)
 
     def train(self):
         # print(1)
